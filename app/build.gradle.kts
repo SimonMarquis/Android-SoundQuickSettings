d4bdbb4372faf85a0a17ae9e60f0a7a583ef2a2c
Simon Marquis <contact@simon-marquis.fr>
2026-07-03 21:46:54 +0200

    Remove unnecessary proguard configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,6 @@ android {
     buildTypes {
         getByName("release") {
             isMinifyEnabled = true
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     compileOptions {
